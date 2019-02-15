d9e83ba9884996da26e0dc7a1562d91ed9472085
Godwin C <46739831+JGodwin25@users.noreply.github.com>
2019-02-15 15:15:28 +0530

    Base Commit
    100% working Hello world ready to implement maven.

---
+++ HelloWorld Java/src/main/java/com/boilerplate/HelloWorld/App.java
@@ -0,0 +1,13 @@
+package com.boilerplate.HelloWorld;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
