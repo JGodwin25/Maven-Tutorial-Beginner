a7fc5fe11b3200dc7b73d20027f0c704990bc5b6
Godwin C <46739831+JGodwin25@users.noreply.github.com>
2019-02-15 16:39:14 +0530

    Base Commit3
    Completed

---
+++ Maven Project Documents/src/main/java/com/boilerplate/HelloWorld/App.java
@@ -0,0 +1,14 @@
+package com.boilerplate.HelloWorld;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World from App !" );
+        Util.printMessage("Jesus Christ is my Saviour");
+    }
+}
---
+++ Maven Project Documents/src/main/java/com/boilerplate/HelloWorld/Util.java
@@ -0,0 +1,8 @@
+package com.boilerplate.HelloWorld;
+
+public class Util{
+
+public static void printMessage(String message){
+System.out.println("Message From Util ! "+message);
+}
+}
